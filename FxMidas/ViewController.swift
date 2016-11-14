177f8cfc3b777758924bc06f77a61a545cfb97a7
KOBOKKYUNG <psioneer@client.openvpn.net>
2016-11-14 20:48:57 +0900

    Initial Commit

---
+++ FxMidas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FxMidas
+//
+//  Created by KOBOKKYUNG on 2016. 11. 14..
+//  Copyright © 2016년 Studio Psion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
